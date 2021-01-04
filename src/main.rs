186b94149e331c8d78902419811f7e0cf8de5afd
gjf2a <ferrer@hendrix.edu>
2021-01-04 12:50:06 -0600

    Initial commit. Basic functionality works.

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+#![no_std]
+#![no_main]
+
+// Based on Writing an OS in Rust (https://os.phil-opp.com/)
+// Adapted by Gabriel Ferrer
+
+mod vga_buffer;
+mod serial;
+
+use pc_keyboard::DecodedKey;
+use pluggable_interrupt_os::handler_table::HandlerTable;
+
+fn tick() {
+    print!(".");
+}
+
+fn key(key: DecodedKey) {
+    match key {
+        DecodedKey::Unicode(character) => print!("{}", character),
+        DecodedKey::RawKey(key) => print!("{:?}", key),
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    let handlers = HandlerTable::new()
+        .keyboard(key)
+        .timer(tick);
+    pluggable_interrupt_os::init(handlers);
+    pluggable_interrupt_os::hlt_loop();
+}
