dc29c4f84c13016271a4bd966fb5cbab2e4ab0d1
gjf2a <ferrer@hendrix.edu>
2021-02-07 05:02:05 -0600

    Removed error code output from Double Fault, as the code is always zero.

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -41,9 +41,9 @@ extern "x86-interrupt" fn breakpoint_handler(
 }
 
 extern "x86-interrupt" fn double_fault_handler(
-    stack_frame: &mut InterruptStackFrame, error_code: u64) -> !
+    stack_frame: &mut InterruptStackFrame, _error_code: u64) -> !
 {
-    panic!("EXCEPTION: DOUBLE FAULT ({})\n{:#?}", error_code, stack_frame);
+    panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
 }
 
 const PIC_1_OFFSET: u8 = 32;
