171d152afd5b0bba40be16700fcad888e674b623
gjf2a <ferrer@hendrix.edu>
2025-04-24 15:46:55 -0500

    Update double fault handler to include error code.

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -41,9 +41,9 @@ extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
 
 extern "x86-interrupt" fn double_fault_handler(
     stack_frame: InterruptStackFrame,
-    _error_code: u64,
+    error_code: u64,
 ) -> ! {
-    panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
+    panic!("EXCEPTION: DOUBLE FAULT (code {error_code})\n{:#?}", stack_frame);
 }
 
 const PIC_1_OFFSET: u8 = 32;
