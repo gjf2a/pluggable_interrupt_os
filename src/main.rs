741aa8275b1592237df6bb8fe1b5574c659adee0
gjf2a <ferrer@hendrix.edu>
2025-05-13 05:38:14 -0500

    Starting experiment with no_panic.

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,19 @@ mod vga_buffer;
 
 use pc_keyboard::DecodedKey;
 use pluggable_interrupt_os::HandlerTable;
+use no_panic::no_panic;
 
+#[no_panic]
 fn start() {
     println!("Hello, world!");
 }
 
+#[no_panic]
 fn tick() {
     print!(".");
 }
 
+#[no_panic]
 fn key(key: DecodedKey) {
     match key {
         DecodedKey::Unicode(character) => print!("{}", character),
@@ -23,6 +27,7 @@ fn key(key: DecodedKey) {
 }
 
 #[no_mangle]
+#[no_panic]
 pub extern "C" fn _start() -> ! {
     HandlerTable::new()
         .keyboard(key)
